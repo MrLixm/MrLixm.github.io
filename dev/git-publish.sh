66c4c989b2a084219f83b356b54edd691734e034
Liam Collod <lcollod@gmail.com>
2021-10-20 21:42:35 +0200

    added publish sh file

---
+++ dev/git-publish.sh
@@ -0,0 +1,14 @@
+read -p "This will publish the output directory to github that will update the web-page, would you like to continue ?
+(y/n): " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+cd ..
+echo $PWD
+
+pelican content  # build to the /output directory using pelicanconf.py
+
+ghp-import output
