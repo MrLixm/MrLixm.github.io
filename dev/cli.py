147a6d4e38dfc33d4175bee0df947e94596984bc
Liam Collod <lcollod@gmail.com>
2022-12-30 13:59:20 +0100

    [dev.cli] added preview command, cleaned other command

--- dev/cli.py
+++ dev/cli.py
@@ -2,26 +2,31 @@ import os
 import logging
 
 import click
-from . import c
-from .publish import interactive_publish
+import pelican
+
+import dev.c
+import dev.publish
 
 logger = logging.getLogger(__name__)
 
 
-@click.group(invoke_without_command=True)
-@click.version_option("{}".format(c.__version__))
+@click.group()
+@click.version_option("{}".format(dev.c.__version__))
 @click.option(
     "--debug",
     is_flag=True,
-    envvar=c.Env.logs_debug.value,
+    envvar=dev.c.Env.logs_debug.value,
     help="This will enable the debug mode which display more informations and disable some features.",
     # --debug is used via sys.argv across the app, do not remove.
 )
-def cli(debug: bool):
+@click.pass_context
+def cli(ctx: click.Context, debug: bool):
     """
     Developer tools for working and editing this blog.
     """
     logger.debug(f"[cli] Started.(cwd={os.getcwd()})[{debug=}]")
+    ctx.ensure_object(dict)
+    ctx.obj["debug"] = debug
     return
 
 
@@ -36,4 +41,19 @@ def publish(dry_run: bool):
     """
     Make the blog available online by building and pushing it to the remote.
     """
-    interactive_publish(dry_run=dry_run)
+    dev.publish.interactive_publish(dry_run=dry_run)
+
+
+@cli.command()
+@click.pass_context
+def preview(ctx: click.Context):
+    """
+    Build the website and create a local server to preview it.
+    The build is auto-refreshed for changes.
+    """
+    if ctx.obj.get("debug"):
+        pelican.main(["--verbose"])
+
+    pelican.main(["--delete-output-directory"])
+    pelican.main(["content"])
+    pelican.main(["--autoreload", "--listen"])
