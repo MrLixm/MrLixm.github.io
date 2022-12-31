9857c962372d784b61e466db3dc499f8a2e96668
Liam Collod <lcollod@gmail.com>
2022-12-31 11:37:38 +0100

    [dev] upgraded to 0.1.2

--- dev/c.py
+++ dev/c.py
@@ -12,7 +12,7 @@ from typing import Any
 
 __version_major__ = 0
 __version_minor__ = 1
-__version_patch__ = 1
+__version_patch__ = 2
 __version__ = f"{__version_major__}.{__version_minor__}.{__version_patch__}"
 if __version_prerelease__ := "":
     __version__ += f"-{__version_prerelease__}"
