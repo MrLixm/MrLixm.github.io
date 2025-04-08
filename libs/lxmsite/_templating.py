4ca59d740f7de4bef2fdf2a15584fb8d2e141fdf
Liam Collod <lcollod@gmail.com>
2025-04-08 22:00:38 +0200

    refacto: improve rel url handling

--- libs/lxmsite/_templating.py
+++ libs/lxmsite/_templating.py
@@ -1,6 +1,8 @@
 import dataclasses
 import datetime
 import logging
+import os
+from pathlib import Path
 
 import jinja2
 
@@ -11,47 +13,64 @@ from ._utils import slugify
 LOGGER = logging.getLogger(__name__)
 
 
-def get_jinja_env(site_config: SiteConfig) -> jinja2.Environment:
+def mkpagerel(path: str, page_path: str) -> str:
+    """
+    Convert a site-root-relative path, to a path relative to the given page.
+    """
+    root_path: Path = Path("placeholder")
+    pageabs = root_path.joinpath(page_path).resolve()
+    pathabs = root_path.joinpath(path).resolve()
+    return Path(os.path.relpath(pathabs, start=pageabs.parent)).as_posix()
 
-    def expand_siteurl(path_: str) -> str:
-        """
-        Add the site url in front of the string
-        """
-        if site_config.PUBLISH_MODE:
-            return f"{site_config.SITE_URL}/{path_}"
-        return path_
 
-    def mksiteabsurl(path_: str, page_url_: str) -> str:
-        """
-        Make a page-relative url, absolute to the site; only in publish mode.
-        """
-        if site_config.PUBLISH_MODE:
-            page_abs = site_config.SRC_ROOT.joinpath(page_url_).parent
-            path_abs = page_abs.joinpath(path_).resolve()
-            path_abs = path_abs.relative_to(site_config.SRC_ROOT)
-            return f"{site_config.SITE_URL}/{path_abs.as_posix()}"
-        return path_
+def mksiterel(path: str, page_path: str) -> str:
+    """
+    Convert a page-relative path to a path relative to the site root.
+    """
+    root_path: Path = Path("placeholder")
+    pageabs = root_path.joinpath(page_path).resolve()
+    pathabs = pageabs.parent.joinpath(path).resolve()
+    return Path(os.path.relpath(pathabs, start=root_path)).as_posix()
 
-    def format_link(link_: str) -> str:
+
+def get_jinja_env(
+    site_config: SiteConfig,
+    page_rel_url: str,
+) -> jinja2.Environment:
+
+    def _mksiteabs_(_path_: str) -> str:
+        """
+        Convert the given site-relative url to absolute.
+        """
+        return f"{site_config.SITE_URL}/{_path_}"
+
+    def _mksiterel_(_path_: str) -> str:
+        return mksiterel(_path_, page_rel_url)
+
+    def _mkpagerel_(_path_: str) -> str:
+        return mkpagerel(_path_, page_rel_url)
+
+    def _format_link_(_link_: str) -> str:
         """
         Make site cross-linking prettier by removing file format suffix on publish.
         """
         # XXX: this works on GitHub pages !!! no guarantee for other host
         if site_config.PUBLISH_MODE:
-            if link_.endswith("index.html"):
-                formatted = link_.removesuffix("index.html")
+            if _link_.endswith("index.html"):
+                formatted = _link_.removesuffix("index.html")
                 return formatted if formatted else "."
-            return link_.removesuffix(".html")
-        return link_
+            return _link_.removesuffix(".html")
+        return _link_
 
     jinja_env = jinja2.Environment(
         undefined=jinja2.StrictUndefined,
         loader=jinja2.FileSystemLoader(site_config.TEMPLATES_ROOT),
     )
     jinja_env.filters["slugify"] = slugify
-    jinja_env.filters["expand_siteurl"] = expand_siteurl
-    jinja_env.filters["mksiteabsurl"] = mksiteabsurl
-    jinja_env.filters["format_link"] = format_link
+    jinja_env.filters["mksiteabs"] = _mksiteabs_
+    jinja_env.filters["mksiterel"] = _mksiterel_
+    jinja_env.filters["mkpagerel"] = _mkpagerel_
+    jinja_env.filters["prettylink"] = _format_link_
     return jinja_env
 
 
@@ -67,13 +86,15 @@ def render_page(
     site_config: SiteConfig,
     context: SiteGlobalContext,
 ) -> str:
-    jinja_env = get_jinja_env(site_config)
+    jinja_env = get_jinja_env(
+        site_config=site_config,
+        page_rel_url=page.url_path,
+    )
     template = jinja_env.get_template(template_name)
     attributes = {
         "Page": page,
         "Config": site_config,
         "Context": context,
-        "URL_PATH": page.url_path,
     }
     content = template.render(**attributes)
     return content
