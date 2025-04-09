6384abf0b371fc7916f6dd50538a2bf502321b27
Liam Collod <lcollod@gmail.com>
2025-04-09 20:25:13 +0200

    refacto(rstlib): convert extensions module as package

--- libs/lxmsite/rstlib/__init__.py
+++ libs/lxmsite/rstlib/__init__.py
@@ -2,6 +2,6 @@ from ._read import read_rst
 from ._read import parse_metadata
 from ._read import DocumentType
 
-from ._extensions import _register_extensions
+from . import _extensions
 
-_register_extensions()
+_extensions.register_extensions()
